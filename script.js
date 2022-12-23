7c0be0cf432e6ec41359454fd548787f7bfc5c58
Alex <alex.kastsiukovich@gmail.com>
2022-12-24 01:07:52 +0300

    blog

---
+++ script.js
@@ -0,0 +1,16 @@
+document.body.innerHTML = `
+<div class='wrapper center'>
+<header>
+   <img src='/images/logo.svg' alt='logo'>
+   <a href='#'><img class='flag' src='/images/header-flag.svg' alt='flag'>Stand with <span>Ukraine</span></a>
+   <ul class='head-ul'>
+   <li><a class='grey' href='#'>What we do</a></li>
+   <li><a class='green' href='#'>Career</a></li>
+   <li><a class='grey' href='#'>Contact</a></li>
+   <li><a class='black' href='#'>Blog</a></li>
+   </ul>
+</header>
+</div>
+
+`;
+
